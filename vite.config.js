dba844281fa3f466e8b904b1dd9e45a51a79af46
Dimitri Bilenkin <dimitribilenkin@dimitris-mbp.mynetworksettings.com>
2025-02-02 23:42:36 -0500

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/connections/",
   server: {
     host: true, // 👈 Allows access from other devices
     port: 5173, // 👈 Optional: Set a specific port
